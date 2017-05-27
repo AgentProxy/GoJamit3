695f7f8f92cbec6087f5f9660e92fe81169bdf8e
Eric Joseph Flores <ericjoseph.flores1@gmail.com>
2017-05-27 12:48:37 +0800

    Discover Controller

---
+++ storage/framework/views/08978cc58a2bbbc54a3d77edc3851b60763cf254.php
@@ -0,0 +1,28 @@
+<?php $__env->startSection('content'); ?>
+ 	<div class="row">
+	    <?php $__empty_1 = true; $__currentLoopData = $notifications; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $notification): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
+	        <div class="col-md-8 col-md-offset-2">
+	            <div class="post-content-panel" id="notif-<?php echo e($notification->id); ?>">
+	                <div class="post-content-body col-xs-12">
+                            <div class="panel panel-default">
+								<!-- <div class="panel-heading"></div> -->
+							 	<div class="panel-body">
+							 	<?php if($notification->type == "1"): ?>
+	                        		<a href="/profile/<?php echo e($user->where('id',$notification->notifier_id)->value('username')); ?>/about"><?php echo e($user->where('id',$notification->notifier_id)->value('username')); ?> </a> has followed you!
+                          		<?php elseif($notification->type == "2"): ?>
+                                	<a href="/profile/<?php echo e($user->where('id',$notification->notifier_id)->value('username')); ?>/about"><?php echo e($user->where('id',$notification->notifier_id)->value('username')); ?> </a> has liked your <a href="/post/<?php echo e($notification->notif_id); ?>">post</a>
+                                	<!-- <a href="/profile/<?php echo e($user->where('id',$notification->notifier_id)->value('username')); ?>/about"><?php echo e($user->where('id',$notification->notifier_id)->value('username')); ?> </a> has liked your <a href="/profile/<?php echo e(Auth::user()->username); ?>/posts/#post-<?php echo e($notification->notif_id); ?>">post</a> -->
+                            	<?php else: ?>
+                                	<a href="/profile/<?php echo e($user->where('id',$notification->notifier_id)->value('username')); ?>/about"><?php echo e($user->where('id',$notification->notifier_id)->value('username')); ?> </a> has commented on your <a href="/post/<?php echo e($notification->notif_id); ?>">post</a>
+                           		<?php endif; ?></div>
+							</div>
+	                    
+	                </div>
+	            </div>
+	        </div>
+	    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
+	    	No new notifications!
+	    <?php endif; ?>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
