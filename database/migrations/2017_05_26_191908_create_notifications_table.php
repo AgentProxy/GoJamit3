b40a19981c2efefae8658362bfda55a54c6f6cd4
AgentProxy <ericjoseph.flores1@gmail.com>
2017-05-28 10:58:37 +0800

    Add files via upload

---
+++ database/migrations/2017_05_26_191908_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id')->unsigned();
+            $table->integer('notif_id')->unsigned();
+            $table->integer('notifier_id')->unsigned();
+            $table->boolean('seen');
+            $table->integer('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
